e9d0451b0e06cc9eeb234f6a2cbbbb58ba34a211
dliu12 <liu@cooper.edu>
2021-10-31 20:41:14 -0400

    Change Api call

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import logo from './logo.svg'
-import './App.css'
-import axios from 'axios'
-import React from 'react'
-const KEY = 'ohg5Z0I2qocabN6TrIojwkyo48DXZa8B6e3aPsPX'
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import React from 'react';
+const KEY = 'ohg5Z0I2qocabN6TrIojwkyo48DXZa8B6e3aPsPX';
 
 function App() {
-    const [formSubmit, setFormSubmit] = React.useState(false)
-    const [imageSrc, setImageSrc] = React.useState()
+  const [formSubmit, setFormSubmit] = React.useState(false);
+  const [imageSrc, setImageSrc] = React.useState();
 
-    const formAction = e => {
-        e.preventDefault()
-        setFormSubmit(true)
+  const formAction = (e) => {
+    e.preventDefault();
+    setFormSubmit(true);
+  };
+
+  React.useEffect(() => {
+    if (formSubmit === true) {
+      axios
+        .get(
+          `https: //api.nasa.gov/insight_weather/?api_key=${KEY}&feedtype=json&ver=1.0`
+        )
+        .then((res) => {
+          console.log(res);
+        });
+      setFormSubmit(false);
     }
+  }, [formSubmit]);
 
-    React.useEffect(() => {
-        if (formSubmit === true) {
-            axios.get(`https://api.nasa.gov/planetary/apod?api_key=${KEY}`).then(res => {
-                setImageSrc(res.data.url)
-                console.log('called')
-            })
-            setFormSubmit(false)
-        }
-    }, [formSubmit])
-
-    return (
-        <div className="App">
-            <form onSubmit={formAction}>
-                <button type="submit">Generate</button>
-            </form>
-            <div>
-                <img src={imageSrc} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="App">
+      <form onSubmit={formAction}>
+        <button type="submit">Generate</button>
+      </form>
+      <div>
+        <img src={imageSrc} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
